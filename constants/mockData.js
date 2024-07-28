33f0e42679a3a4ec31a8f28e93991de786b68b47
Slew-Code <sm.lewin10@gmail.com>
2024-07-28 17:20:13 +0100

    Add Demo gif to asset images for use in ReadMe

--- constants/mockData.js
+++ constants/mockData.js
@@ -1,43 +1,15 @@
 const mockData = [
-    /*
-    {
-            "brand": "adidas",
-            "colorway": "Core Black/Cloud White/Chalk White",
-            "estimatedMarketValue": 74,
-            "gender": "men",
-            "id": "cc25575e-b235-46bc-b12e-6a66633c3ddf",
-            "image": {
-                "360": [],
-                "original": "https://image.goat.com/attachments/product_template_pictures/images/098/451/956/original/1280492_00.png.png",
-                "small": "https://image.goat.com/750/attachments/product_template_pictures/images/098/451/956/original/1280492_00.png.png",
-                "thumbnail": "https://image.goat.com/375/attachments/product_template_pictures/images/098/451/956/original/1280492_00.png.png"
-            },
-            "links": {
-                "flightClub": "https://flightclub.com/crazy-iiinfinity-white-black-ig6303",
-                "goat": "https://goat.com/sneakers/crazy-iiinfinity-white-black-ig6303",
-                "stadiumGoods": "",
-                "stockX": "https://stockx.com/adidas-crazy-iiinfinity-stormtrooper"
-            },
-            "name": "adidas Crazy IIInfinity Stormtrooper",
-            "releaseDate": "2024-12-30",
-            "releaseYear": "2024",
-            "retailPrice": 160,
-            "silhouette": "Crazy IIInfinity",
-            "sku": "IG6303",
-            "story": "The adidas Crazy IIInfinity ‘Stormtrooper’ presents a retro-inspired lifestyle silhouette that takes design cues from the Crazy 1, formerly known as The Kobe. The upper features a zippered shroud in black textile, contrasted by white molded foam paneling with debossed three-stripes at the midfoot. A reinforced rubber toe cap gives a nod to The Kobe 2, along with adidas’ shell-toe heritage. Underfoot, the herringbone-traction rubber outsole reveals an exposed TPU shank for midfoot stability."
-        },
-    */
     {
         "brand": "adidas",
         "colorway": "Core Black/Cloud White/Chalk White",
         "estimatedMarketValue": 74,
         "gender": "men",
-        "id": "8838d55a-6579-4f6c-83d1-48716495d597",
+        "id": "cc25575e-b235-46bc-b12e-6a66633c3ddf",
         "image": {
-                "360": [],
-                "original": "https://image.goat.com/375/attachments/product_template_pictures/images/079/484/521/original/508079_00.png.png",
-                "small": "https://image.goat.com/375/attachments/product_template_pictures/images/079/484/521/original/508079_00.png.png",
-                "thumbnail": "https://image.goat.com/375/attachments/product_template_pictures/images/098/451/956/original/1280492_00.png.png"
+            "360": [],
+            "original": "https://image.goat.com/attachments/product_template_pictures/images/098/451/956/original/1280492_00.png.png",
+            "small": "https://image.goat.com/750/attachments/product_template_pictures/images/098/451/956/original/1280492_00.png.png",
+            "thumbnail": "https://image.goat.com/375/attachments/product_template_pictures/images/098/451/956/original/1280492_00.png.png"
         },
         "links": {
             "flightClub": "https://flightclub.com/crazy-iiinfinity-white-black-ig6303",
@@ -52,92 +24,137 @@ const mockData = [
         "silhouette": "Crazy IIInfinity",
         "sku": "IG6303",
         "story": "The adidas Crazy IIInfinity ‘Stormtrooper’ presents a retro-inspired lifestyle silhouette that takes design cues from the Crazy 1, formerly known as The Kobe. The upper features a zippered shroud in black textile, contrasted by white molded foam paneling with debossed three-stripes at the midfoot. A reinforced rubber toe cap gives a nod to The Kobe 2, along with adidas’ shell-toe heritage. Underfoot, the herringbone-traction rubber outsole reveals an exposed TPU shank for midfoot stability."
-    }
-    /*
-    {
-        id: 2,
-        image: 'https://image.goat.com/attachments/product_template_pictures/images/079/487/442/original/560709_00.png.png',
-        title: 'Yeezy Slides Resin',
-        story: 'Description for Card 1',
-        estimatedMarketValue: 99,
-        links: [
-            "https://stockx.com/air-jordan-1-retro-high-bloodline",
-            "https://goat.com/sneakers/air-jordan-1-high-retro-og-bred-2019-555088-062",  //no affiliate 
-            "https://flightclub.com/air-jordan-1-retro-high-og-black-gym-red-white-140229", //no affiliate
-            "https://sale.stadiumgoods.com/air-jordan-1-high-og-bloodline-meant-to-fly-555088-062"
-        ]
-    },
-    {
-        id: 3,
-        image: 'https://image.goat.com/375/attachments/product_template_pictures/images/081/096/394/original/616017_00.png.png',
-        title: 'Nike SB Dunk Low Ben & Jerry\'s Chunky Dunky',
-        description: 'Description for Card 2',
-        estimatedMarketValue: 1185,
-        links: [
-            "https://stockx.com/nike-sb-dunk-low-ben-jerrys-chunky-dunky",
-            "",  //no affiliate 
-            "", //no affiliate
-            "https://sale.stadiumgoods.com/nike-sb-dunk-low-ben-jerry-s-chunky-dunky-cu3244-100"
-        ]
-    },
-    {
-        id: 4,
-        image: "https://image.goat.com/375/attachments/product_template_pictures/images/033/250/439/original/BZ0028.png.png",
-        title: 'Adidas Gazelle Blue',
-        description: 'Description for Card 3',
-        estimatedMarketValue: 80,
-        //link: "https://stockx.com/adidas-gazelle-blue"
-        links: [
-            "https://stockx.com/air-jordan-1-retro-high-bloodline",
-            "https://goat.com/sneakers/air-jordan-1-high-retro-og-bred-2019-555088-062",  //no affiliate 
-            "https://flightclub.com/air-jordan-1-retro-high-og-black-gym-red-white-140229", //no affiliate
-            "https://sale.stadiumgoods.com/air-jordan-1-high-og-bloodline-meant-to-fly-555088-062"
-        ]
-    }   
-    */           
-];
-
-export default mockData;
-
-
-
-/*
-const mockData = [
-    {
-        "brand": "adidas",
-        "colorway": "Core Black/Cloud White/Chalk White",
-        "gender": "men",
-        "id": "af7a25fb-0a8c-42c7-ae78-6356573af951",
-        "media": {
-            "imageUrl": "https://images.stockx.com/images/adidas-Crazy-IIInfinity-Stormtrooper.jpg?fit=fill&bg=FFFFFF&w=700&h=500&fm=webp&auto=compress&trim=color&q=90&dpr=2&updated_at=1704212941",
-            "smallImageUrl": "https://images.stockx.com/images/adidas-Crazy-IIInfinity-Stormtrooper.jpg?fit=fill&bg=FFFFFF&w=300&h=214&fm=webp&auto=compress&trim=color&q=90&dpr=2&updated_at=1704212941",
-            "thumbUrl": "https://images.stockx.com/images/adidas-Crazy-IIInfinity-Stormtrooper.jpg?fit=fill&bg=FFFFFF&w=140&h=100&fm=webp&auto=compress&trim=color&q=90&dpr=2&updated_at=1704212941",
-        },
-        "name": "Stormtrooper",
-        "releaseDate": "2024-12-30",
-        "retailPrice": 160,
-        "shoe": "adidas Crazy IIInfinity",
-        "styleId": "IG6303",
-        "title": "adidas Crazy IIInfinity Stormtrooper",
-        "year": 2024,
     },
     {
         "brand": "adidas",
         "colorway": "Off-White/Black/Metallic Gold",
+        "estimatedMarketValue": 110,
         "gender": "men",
-        "id": "8838d55a-6579-4f6c-83d1-48716495d597",
-        "media": {
-            "imageUrl": "https://images.stockx.com/images/adidas-Harden-Vol-7-Year-of-the-Dragon-2024.jpg?fit=fill&bg=FFFFFF&w=700&h=500&fm=webp&auto=compress&trim=color&q=90&dpr=2&updated_at=1704212971",
-            "smallImageUrl": "https://images.stockx.com/images/adidas-Harden-Vol-7-Year-of-the-Dragon-2024.jpg?fit=fill&bg=FFFFFF&w=300&h=214&fm=webp&auto=compress&trim=color&q=90&dpr=2&updated_at=1704212971",
-            "thumbUrl": "https://images.stockx.com/images/adidas-Harden-Vol-7-Year-of-the-Dragon-2024.jpg?fit=fill&bg=FFFFFF&w=140&h=100&fm=webp&auto=compress&trim=color&q=90&dpr=2&updated_at=1704212971",
+        "id": "be58449f-1866-46c7-93ba-4b4db96cb291",
+        "image": {
+            "360": [],
+            "original": "https://image.goat.com/attachments/product_template_pictures/images/096/506/877/original/IH7516.png.png",
+            "small": "https://image.goat.com/750/attachments/product_template_pictures/images/096/506/877/original/IH7516.png.png",
+            "thumbnail": "https://image.goat.com/375/attachments/product_template_pictures/images/096/506/877/original/IH7516.png.png"
         },
-        "name": "Year of the Dragon (2024)",
+        "links": {
+            "flightClub": "https://flightclub.com/harden-vol-7-year-of-the-dragon-ih7516",
+            "goat": "https://goat.com/sneakers/harden-vol-7-year-of-the-dragon-ih7516",
+            "stadiumGoods": "",
+            "stockX": "https://stockx.com/adidas-harden-vol-7-year-of-the-dragon-2024"
+        },
+        "name": "adidas Harden Vol. 7 Year of the Dragon (2024)",
         "releaseDate": "2024-12-29",
+        "releaseYear": "2024",
         "retailPrice": 160,
-        "shoe": "adidas Harden Vol. 7",
-        "styleId": "IH7516",
-        "title": "adidas Harden Vol. 7 Year of the Dragon (2024)",
-        "year": 2024,
-    }
+        "silhouette": "Harden Vol. 7",
+        "sku": "IH7516",
+        "story": ""
+    },
+    {
+        "brand": "adidas",
+        "colorway": "Core White/Matte Gold/Halo Gold",
+        "estimatedMarketValue": 71,
+        "gender": "men",
+        "id": "7bd701b1-9c21-4c1a-9569-4165b9df529c",
+        "image": {
+            "360": [],
+            "original": "https://image.goat.com/attachments/product_template_pictures/images/096/421/316/original/IH7517.png.png",
+            "small": "https://image.goat.com/750/attachments/product_template_pictures/images/096/421/316/original/IH7517.png.png",
+            "thumbnail": "https://image.goat.com/375/attachments/product_template_pictures/images/096/421/316/original/IH7517.png.png"
+        },
+        "links": {
+            "flightClub": "https://flightclub.com/d-o-n-issue-5-year-of-the-dragon-ih7517",
+            "goat": "https://goat.com/sneakers/d-o-n-issue-5-year-of-the-dragon-ih7517",
+            "stadiumGoods": "",
+            "stockX": "https://stockx.com/adidas-don-issue-5-year-of-the-dragon-2024"
+        },
+        "name": "adidas D.O.N. Issue #5 Year of the Dragon (2024)",
+        "releaseDate": "2024-12-27",
+        "releaseYear": "2024",
+        "retailPrice": 120,
+        "silhouette": "D.O.N. Issue #5",
+        "sku": "IH7517",
+        "story": ""
+    },
+    {
+        "brand": "Jordan",
+        "colorway": "White/Black/Sail/Metallic Silver",
+        "estimatedMarketValue": 200,
+        "gender": "men",
+        "id": "82067298-ec7a-48eb-b5cf-94f860b278e0",
+        "image": {
+            "360": [],
+            "original": "https://image.goat.com/attachments/product_template_pictures/images/101/132/580/original/DD0587_110.png.png",
+            "small": "https://image.goat.com/750/attachments/product_template_pictures/images/101/132/580/original/DD0587_110.png.png",
+            "thumbnail": "https://image.goat.com/375/attachments/product_template_pictures/images/101/132/580/original/DD0587_110.png.png"
+        },
+        "links": {
+            "flightClub": "https://flightclub.com/air-jordan-5-retro-white-black-dd0587-110",
+            "goat": "https://goat.com/sneakers/air-jordan-5-retro-white-black-dd0587-110",
+            "stadiumGoods": "",
+            "stockX": "https://stockx.com/air-jordan-5-retro-reverse-metallic"
+        },
+        "name": "Jordan 5 Retro Reverse Metallic",
+        "releaseDate": "2024-12-07",
+        "releaseYear": "2024",
+        "retailPrice": 200,
+        "silhouette": "Air Jordan 5",
+        "sku": "DD0587-110",
+        "story": ""
+    },  
+    {
+        "brand": "Jordan",
+        "colorway": "White/Legend Blue/Black",
+        "estimatedMarketValue": 185,
+        "gender": "child",
+        "id": "27822880-d0b8-44f6-84a1-bc2364140caf",
+        "image": {
+            "360": [],
+            "original": "",
+            "small": "",
+            "thumbnail": ""
+        },
+        "links": {
+            "flightClub": "",
+            "goat": "https://goat.com/sneakers/air-jordan-11-retro-gs-columbia-legend-blue-2024-378038-104",
+            "stadiumGoods": "",
+            "stockX": "https://stockx.com/air-jordan-11-retro-legend-blue-2024-gs"
+        },
+        "name": "Jordan 11 Retro Legend Blue (2024) (GS)",
+        "releaseDate": "2024-12-14",
+        "releaseYear": "2024",
+        "retailPrice": 185,
+        "silhouette": "Air Jordan 11",
+        "sku": "378038-104",
+        "story": "Built for big kids, the 2024 reissue of the Air Jordan 11 Retro GS &#39;Columbia / Legend Blue&#39; brings back an OG colorway. Laces feed through webbing eyelets to secure the white leather upper, featuring a tonal mudguard in shiny patent leather. A Jumpman logo graces the lateral collar in light blue embroidery, while a &#39;23&#39; decorates the contrasting black back tab. The cushioning setup comprises a lightweight Phylon midsole with a full-length Air-sole unit. Underfoot, a blue-tinged translucent rubber outsole provides durable traction."
+    },    
+    {
+        "brand": "Nike",
+        "colorway": "Luminous Green/Crimson Tint/Volt/Black",
+        "estimatedMarketValue": 168,
+        "gender": "men",
+        "id": "81fe7efa-748b-4400-9e19-3d1fd2e0ae6d",
+        "image": {
+            "360": [],
+            "original": "https://image.goat.com/attachments/product_template_pictures/images/095/591/040/original/FQ8109_331.png.png",
+            "small": "https://image.goat.com/750/attachments/product_template_pictures/images/095/591/040/original/FQ8109_331.png.png",
+            "thumbnail": "https://image.goat.com/375/attachments/product_template_pictures/images/095/591/040/original/FQ8109_331.png.png"
+        },
+        "links": {
+            "flightClub": "https://flightclub.com/zoomx-vaporfly-next-3-ekiden-zoom-pack-fq8109-331",
+            "goat": "https://goat.com/sneakers/zoomx-vaporfly-next-3-ekiden-zoom-pack-fq8109-331",
+            "stadiumGoods": "",
+            "stockX": "https://stockx.com/nike-zoomx-vaporfly-3-ekiden-pack"
+        },
+        "name": "Nike ZoomX Vaporfly 3 Ekiden Pack",
+        "releaseDate": "2024-12-01",
+        "releaseYear": "2024",
+        "retailPrice": 270,
+        "silhouette": "ZoomX VaporFly Next% 3",
+        "sku": "FQ8109-331",
+        "story": ""
+    },
 ];
-*/
+
+export default mockData;
